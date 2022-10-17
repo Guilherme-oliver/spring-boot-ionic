809523d5f0205be3ed73ee30020d9018b8ea25c4
Guilherme <guilherme.rodriguess.oliveira@gmail.com>
2022-10-17 18:34:46 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/guilhermeoliveira/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/guilhermeoliveira/cursomc/repositories/ProdutoRepository.java
@@ -1,12 +1,19 @@
 package com.guilhermeoliveira.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.guilhermeoliveira.cursomc.domain.Categoria;
 import com.guilhermeoliveira.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
 	
-	
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRquest);
 }
